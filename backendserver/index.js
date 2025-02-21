d4814b348ba2f91a2a251d1d927fa1e849eac551
vivek <sankaliyavivek9797@gmail.com>
2025-02-21 17:49:38 +0530

    ok

--- backendserver/index.js
+++ backendserver/index.js
@@ -3,7 +3,12 @@ const app = express();
 const cors = require('cors');
 require('./connect');
 
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://niramayclinic.netlify.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 app.use(express.json());
 app.use(cors({
