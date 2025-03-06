bcf97c07ca2ddb309c148f592f1707df83e72a91
vivek <sankaliyavivek9797@gmail.com>
2025-03-06 16:24:51 +0530

    ok

--- backendserver/index.js
+++ backendserver/index.js
@@ -15,10 +15,10 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+
 // Set custom headers
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Origin', 'https://niramayclinic.netlify.app/');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
