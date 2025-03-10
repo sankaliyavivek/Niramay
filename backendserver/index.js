b8b456de3b51b04aa345778a5a9910194ac964b9
vivek <sankaliyavivek9797@gmail.com>
2025-03-10 18:13:04 +0530

    ok

--- backendserver/index.js
+++ backendserver/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:9000", "https://niramayclinic.netlify.app"],
+    origin: ["http://localhost:3000", "https://niramayclinic.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
