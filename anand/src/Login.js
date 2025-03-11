764d985626477838587e66e092392d975acc2880
vivek <sankaliyavivek9797@gmail.com>
2025-03-11 14:59:20 +0530

    ok

--- anand/src/Login.js
+++ anand/src/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import axios from 'axios'
 // import { useNavigate } from 'react-router-dom';
 
-const API_URL = process.env.REACT_APP_BACKEND_API_URL || "https://localhost:9000";
+const API_URL = process.env.REACT_APP_BACKEND_API_URL;
 function Login({ onLoginSuccess }) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
