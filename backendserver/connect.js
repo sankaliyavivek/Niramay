e257f6ec6d889a9e01fdfc05c72f719bcd8e0c6c
vivek <sankaliyavivek9797@gmail.com>
2025-03-21 10:57:52 +0530

    ok

--- backendserver/connect.js
+++ backendserver/connect.js
@@ -1,6 +1,10 @@
 const mongooes = require('mongoose');
 
-mongooes.connect('mongodb+srv://sankaliya200310:hVceOULeEGTYdgeY@cluster1.6lmgz.mongodb.net/mypatients')
+mongooes.connect('mongodb+srv://sankaliya200310:hVceOULeEGTYdgeY@cluster1.6lmgz.mongodb.net/mypatients',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
 .then(()=>{console.log('connect!')})
 
 module.exports= mongooes.connect;
