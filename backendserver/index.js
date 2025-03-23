ddf17e3d85527f248044e2efa933063553f33130
vivek <sankaliyavivek9797@gmail.com>
2025-03-23 12:22:02 +0530

    ok

--- backendserver/index.js
+++ backendserver/index.js
@@ -12,6 +12,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: ["http://localhost:3000", "https://niramayclinic.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 }));
 
